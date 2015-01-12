529f729822dc2a38721df266277afc65e67102ea
William Jones <williammasonjones@gmail.com>
2015-01-12 08:12:12 -0500

    Player Setup

--- 01-08/tic-tac-toe.rb
+++ 01-08/tic-tac-toe.rb
@@ -15,4 +15,33 @@ while true do
     board[index] = index.to_s
   end
 
+  # Setup Player 1
+  puts "Player 1: What's your name?"
+  player1_name = gets.chomp
+  unless player1_name.empty?
+    players[:player1][:name] = player1_name
+  end
+  puts "Hello #{players[:player1][:name]}! Please choose a piece ('X' or 'O'):"
+  players = choose_pieces(gets.chomp, players)
+
+  # Setup Player 2
+  puts "Player 2: What's your name? (Leave blank for AI)"
+  player2_name = gets.chomp
+  if player2_name.empty?
+    players[:player2][:ai] = true
+  else
+    players[:player2][:name] = player2_name
+  end
+  puts
+
+  # Let's recap the setup
+  players.each do |key, player|
+    print "#{player[:name]} "
+    if player[:ai]
+      print "(AI) "
+    end
+    print "is '#{player[:piece]}'\n"
+  end
+  puts
+
 end
